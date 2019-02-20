23cee3bedb15e9dd5fa2bd1292c552b2d7a25cce
jweiser <joel.weiser@oicr.on.ca>
2019-02-20 10:30:21 -0500

    Adds logic to prompt for topic files if necessary and refactors

--- src/org/gk/scripts/VersionTopicComparer.java
+++ src/org/gk/scripts/VersionTopicComparer.java
@@ -24,7 +24,9 @@ import java.util.stream.Stream;
  * @author Joel Weiser
  */
 public class VersionTopicComparer {
+	private static final int NUMBER_OF_FILES_TO_COMPARE = 2;
 	private static final Pattern VERSION_TOPIC_FILE_PATTERN = Pattern.compile("^ver(\\d+)_topics.txt$");
+
 
 	public static void main(String[] args) {
 		String[] topicFiles = getTopicFiles(args);
@@ -50,17 +52,29 @@ public class VersionTopicComparer {
 	}
 
 	private static String[] getTopicFiles(String[] args) {
-		final long numOfFilesToCompare = 2;
+		String[] topicFiles = searchForTopicFiles(args);
+		if (topicFiles.length != NUMBER_OF_FILES_TO_COMPARE) {
+			System.out.print("Exactly " + NUMBER_OF_FILES_TO_COMPARE + " version topic files could not be found. ");
+			System.out.println("Please provide their names (along with paths if in another directory) manually.");
+			System.out.println();
 
-		String[] topicFiles = args.length == numOfFilesToCompare ?
-			getTopicFilesFromArgs(args) :
-			getTopicFilesFromCurrentDirectory();
-		if (topicFiles.length == numOfFilesToCompare) {
-			return topicFiles;
+			topicFiles = promptForVersionTopicFiles(new Scanner(System.in));
+			if (topicFiles.length != NUMBER_OF_FILES_TO_COMPARE) {
+				throw new RuntimeException("Unable to find " + NUMBER_OF_FILES_TO_COMPARE + " version topic files");
+			}
+		}
+
+		return topicFiles;
+	}
+
+	private static String[] searchForTopicFiles(String[] args) {
+		// Enough arguments that there is the potential for the
+		// required number of version topic files to compare to
+		// be found
+		if (args.length >= NUMBER_OF_FILES_TO_COMPARE) {
+			return getTopicFilesFromArgs(args);
 		} else {
-			throw new IllegalStateException(
-				"Could not find two version topic files. Found " + Arrays.toString(topicFiles)
-			);
+			return getTopicFilesFromCurrentDirectory();
 		}
 	}
 
@@ -73,7 +87,6 @@ public class VersionTopicComparer {
 		try {
 			return filterAndSortVersionTopicFileNames(
 				Files.list(Paths.get(currentWorkingDirectory))
-					.filter(Files::isRegularFile)
 					.map(file -> file.getFileName().toString())
 			);
 
@@ -82,9 +95,62 @@ public class VersionTopicComparer {
 		}
 	}
 
+	private static String[] promptForVersionTopicFiles(Scanner reader) {
+		String firstVersionTopicFile = promptForVersionTopicFile(
+			reader, "Enter the name of first version topic file: "
+		);
+		String secondVersionTopicFile = promptForVersionTopicFile(
+			reader, "Enter the name of second version topic file: "
+		);
+
+		return filterAndSortVersionTopicFileNames(Stream.of(firstVersionTopicFile, secondVersionTopicFile));
+	}
+
+	private static String promptForVersionTopicFile(Scanner reader, String query) {
+		return promptForVersionTopicFile(reader, query, 1);
+	}
+
+	private static String promptForVersionTopicFile(Scanner reader, String query, int timesCalled) {
+		final int METHOD_CALL_LIMIT = 3; // Attempt prompt 3 times before throwing exception
+
+		System.out.print(query);
+		String versionTopicFile = reader.next();
+
+		Optional<String> error = findVersionTopicFileNameError(versionTopicFile);
+		if (error.isPresent()) {
+			String errorValue = error.get();
+			if (timesCalled < METHOD_CALL_LIMIT) {
+				System.out.println(errorValue);
+				versionTopicFile = promptForVersionTopicFile(reader, query, ++timesCalled);
+			} else {
+				throw new RuntimeException("Unable to obtain version topic file: " + errorValue);
+			}
+		}
+
+		return versionTopicFile;
+	}
+
+	private static Optional<String> findVersionTopicFileNameError(String fileName) {
+		if (!hasCorrectPattern().test(fileName)) {
+			return Optional.of("The version topic file provided must have the pattern " + VERSION_TOPIC_FILE_PATTERN);
+		} else if (!fileExists().test(fileName)) {
+			return Optional.of("The version topic " + fileName + " file can not be found");
+		} else {
+			return Optional.empty();
+		}
+	}
+
+	private static Predicate<String> hasCorrectPattern() {
+		return filePath -> getVersionTopicMatcher(filePath).matches();
+	}
+
+	private static Predicate<String> fileExists() {
+		return filePath -> Files.isRegularFile(Paths.get(filePath));
+	}
+
 	private static String[] filterAndSortVersionTopicFileNames(Stream<String> fileNames) {
 		return fileNames
-			.filter(fileName -> getVersionTopicMatcher(fileName).matches())
+			.filter(hasCorrectPattern().and(fileExists()))
 			.sorted(newestVersionFirst())
 			.toArray(String[]::new);
 	}
