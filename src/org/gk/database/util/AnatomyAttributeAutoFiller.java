ae4218a5536e67232c4a4d1cb8b9ef16a00870c9
Guanming Wu <guanmingwu@gmail.com>
2020-10-13 17:45:08 -0700

    Added support to auto-fetch anatomy ontology term

---
+++ src/org/gk/database/util/AnatomyAttributeAutoFiller.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Sep 12, 2012
+ *
+ */
+package org.gk.database.util;
+
+/**
+ * @author gwu
+ *
+ */
+public class AnatomyAttributeAutoFiller extends DiseaseAttributeAutoFiller {
+    
+    public AnatomyAttributeAutoFiller() {
+        ONTOLOGY_NAME = "UBERON";
+        displayOntologyName = "Anatomy";
+    }
+    
+}
