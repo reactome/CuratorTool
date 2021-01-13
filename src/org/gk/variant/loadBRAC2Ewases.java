ab26981cb4b4941e87a797f670eef5f00d08308f
shaox <shaox@10.0.0.139>
2021-01-12 19:38:00 -0800

    The Java program to load 150 BRAC2 nonsense mutant EWASes. To be refactored for promoting code-reusing.

--- src/org/gk/variant/loadBRAC2Ewases.java
+++ src/org/gk/variant/loadBRAC2Ewases.java
@@ -2,8 +2,11 @@ package org.gk.variant;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.gk.model.GKInstance;
+import org.gk.model.InstanceDisplayNameGenerator;
 import org.gk.model.ReactomeJavaConstants;
 import org.gk.persistence.MySQLAdaptor;
 import org.gk.persistence.PersistenceManager;
@@ -13,8 +16,10 @@ public class loadBRAC2Ewases {
 
 	public static void main(String[] args) throws SQLException {		
         try{
-            String src = "/Users/shaox/eclipse-workspace/CuratorTool/resources/Disease_BRCA2_EWASs.tsv";
-            String dest = "/Users/shaox/Desktop/BRCA2s";
+        	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd");  
+        	LocalDateTime now = LocalDateTime.now();  
+        	String src = "/Users/shaox/eclipse-workspace/CuratorTool/resources/Disease_BRCA2_EWASs.tsv";
+            String dest = "/Users/shaox/Desktop/NonsenseMutants_" + dtf.format(now) + ".rtpj";
 
             BufferedReader reader = new BufferedReader(new FileReader(src));
             
@@ -24,104 +29,87 @@ public class loadBRAC2Ewases {
     		PersistenceManager mngr = PersistenceManager.getManager();
     		
     		MySQLAdaptor dba = new MySQLAdaptor("curator.reactome.org",
-                    "gk_central",
-                    "authortool",
-                    "T001test",
-                    3306);
+                                                "gk_central",
+                                                "authortool",
+                                                "T001test",
+                                                3306);
 
     		mngr.setActiveMySQLAdaptor(dba);
-    		
-            //XMLFileAdaptor fileAdaptor = mngr.getFileAdaptor(dest);
+    		    		
     		XMLFileAdaptor fileAdaptor = new XMLFileAdaptor();
-
+    		mngr.setActiveFileAdaptor(fileAdaptor);
     		
-    		int ct = 1;
-            while(ct < 152) {                     // total number of lines : 151
-                line = reader.readLine();
-                if(line != null && ct > 1) {        
+    		int ct = 0;
+            while((line = reader.readLine()) != null) {             
+                if(ct > 0) {                    // skip the first description line
                 	fileds = line.split("\t");
-                	if(ct == 15) {    // do the first one with 2 cross references(line#15) 
                 		String name = fileds[0];
-                    	System.out.println(name);
                 		GKInstance ewas = fileAdaptor.createNewInstance(ReactomeJavaConstants.EntityWithAccessionedSequence);
-                		ewas.setDisplayName(name);
-                		ewas.setAttributeValueNoCheck(ReactomeJavaConstants.name, name);
+                		ewas.setAttributeValue(ReactomeJavaConstants.name, name);
                         String referenceEntityID = fileds[1];
-                        System.out.println(referenceEntityID);
                         GKInstance referenceEntity = dba.fetchInstance(Long.parseLong(referenceEntityID));
-                        ewas.setAttributeValueNoCheck(ReactomeJavaConstants.referenceEntity, referenceEntity);
+                        GKInstance localReferenceEntity = mngr.download(referenceEntity);
+                        ewas.setAttributeValue(ReactomeJavaConstants.referenceEntity, localReferenceEntity);
                         String start = fileds[2];
-                        System.out.println(start);
                         Integer startCoordinate = Integer.parseInt(start);
-                        ewas.setAttributeValueNoCheck(ReactomeJavaConstants.startCoordinate, startCoordinate);
+                        ewas.setAttributeValue(ReactomeJavaConstants.startCoordinate, startCoordinate);
                         String end = fileds[3];
-                        System.out.println(end);
                         Integer endCoordinate = Integer.parseInt(end);
-                        ewas.setAttributeValueNoCheck(ReactomeJavaConstants.endCoordinate, endCoordinate);
+                        ewas.setAttributeValue(ReactomeJavaConstants.endCoordinate, endCoordinate);
                         String compartmentID = fileds[4];
-                        System.out.println(compartmentID);
                         GKInstance compartment = dba.fetchInstance(Long.parseLong(compartmentID));
-                        ewas.setAttributeValueNoCheck(ReactomeJavaConstants.compartment, compartment);
-                        String hasModifiedResidue = fileds[5];
-                        System.out.println(hasModifiedResidue);
+                        GKInstance localCompartment = mngr.download(compartment);
+                        ewas.setAttributeValue(ReactomeJavaConstants.compartment, localCompartment);
                         GKInstance nonsenseMutation = fileAdaptor.createNewInstance(ReactomeJavaConstants.NonsenseMutation);
                         String coordinate = fileds[6];
-                        System.out.println(coordinate);
                         Integer mutCoordinate = Integer.parseInt(coordinate);
-                        nonsenseMutation.setAttributeValueNoCheck(ReactomeJavaConstants.coordinate, mutCoordinate);
+                        nonsenseMutation.setAttributeValue(ReactomeJavaConstants.coordinate, mutCoordinate);
                         String psiModID = fileds[7];
-                        System.out.println(psiModID);
                         GKInstance psiMod = dba.fetchInstance(Long.parseLong(psiModID));
-                        nonsenseMutation.setAttributeValueNoCheck(ReactomeJavaConstants.psiMod, psiMod);
+                        GKInstance localPsiMod = mngr.download(psiMod);
+                        nonsenseMutation.setAttributeValue(ReactomeJavaConstants.psiMod, localPsiMod);
                         String mutRefSeqID = fileds[8];
-                        System.out.println(mutRefSeqID);
                         GKInstance mutReferenceSequence = dba.fetchInstance(Long.parseLong(mutRefSeqID));
-                        nonsenseMutation.setAttributeValueNoCheck(ReactomeJavaConstants.referenceSequence, mutReferenceSequence);
-                        fileAdaptor.addNewInstance(nonsenseMutation);
+                        GKInstance localMutReferenceSequence = mngr.download(mutReferenceSequence);
+                        nonsenseMutation.setAttributeValue(ReactomeJavaConstants.referenceSequence, localMutReferenceSequence);
+                        String displayNameOfNonsenseMutation = InstanceDisplayNameGenerator.generateDisplayName(nonsenseMutation);
+                        nonsenseMutation.setDisplayName(displayNameOfNonsenseMutation);
                         
-                        ewas.setAttributeValueNoCheck(ReactomeJavaConstants.hasModifiedResidue, nonsenseMutation);
-                        String crossReferenceStr = fileds[9];
-                        System.out.println(crossReferenceStr);
+                        ewas.setAttributeValue(ReactomeJavaConstants.hasModifiedResidue, nonsenseMutation);
                         String referenceDatabaseID = fileds[10];
-                        System.out.println(referenceDatabaseID);
                         GKInstance referenceDatabase = dba.fetchInstance(Long.parseLong(referenceDatabaseID));
+                        GKInstance localMutReferenceDatabase = mngr.download(referenceDatabase);
                         String cosmicIDs = fileds[11];
-                        System.out.println(cosmicIDs);
 
                         String[] cosmicIDsArray = cosmicIDs.split("\\|");
-                        int numCosmicIDs = cosmicIDsArray.length;
-                        System.out.println("numCosmicIDs = " + numCosmicIDs);
                         for(String cosmicID : cosmicIDsArray){
-                        	System.out.println(cosmicID);
-                        	String crossRefName = referenceDatabase.getDisplayName() + ":" + cosmicID;
                         	GKInstance crossReference = fileAdaptor.createNewInstance(ReactomeJavaConstants.DatabaseIdentifier);
-                        	crossReference.setDisplayName(crossRefName);
-                        	crossReference.setAttributeValueNoCheck(ReactomeJavaConstants.referenceDatabase, referenceDatabase);
-                        	crossReference.setAttributeValueNoCheck(ReactomeJavaConstants.identifier, cosmicID);
-                        	fileAdaptor.addNewInstance(crossReference);
+                        	crossReference.setAttributeValue(ReactomeJavaConstants.referenceDatabase, localMutReferenceDatabase);
+                        	crossReference.setAttributeValue(ReactomeJavaConstants.identifier, cosmicID);
+                        	String displaynameCrossRef = InstanceDisplayNameGenerator.generateDisplayName(crossReference);
+                        	crossReference.setDisplayName(displaynameCrossRef);
                         	
-                        	// how to set (add) more than one instances of crossReference to EWAS ?
-                            ewas.setAttributeValueNoCheck(ReactomeJavaConstants.crossReference, crossReference);
+                            ewas.addAttributeValue(ReactomeJavaConstants.crossReference, crossReference);
                         }
                         
                         String diseaseID = fileds[12];
-                        System.out.println(diseaseID);
                         GKInstance disease = dba.fetchInstance(Long.parseLong(diseaseID));
-                        ewas.setAttributeValueNoCheck(ReactomeJavaConstants.disease, disease);                        
+                        GKInstance localDisease = mngr.download(disease);
+                        ewas.setAttributeValueNoCheck(ReactomeJavaConstants.disease, localDisease);                        
                         String speciesID = fileds[13];
-                        System.out.println(speciesID);
-                        GKInstance species = dba.fetchInstance(Long.parseLong(speciesID));    
-                        ewas.setAttributeValueNoCheck(ReactomeJavaConstants.species, species); 
+                        GKInstance species = dba.fetchInstance(Long.parseLong(speciesID)); 
+                        GKInstance localSpecies = mngr.download(species);
+                        ewas.setAttributeValue(ReactomeJavaConstants.species, localSpecies); 
                         
-                        fileAdaptor.addNewInstance(ewas);
-                	}
+                        String displayNameOfEwas = InstanceDisplayNameGenerator.generateDisplayName(ewas);
+                        ewas.setDisplayName(displayNameOfEwas);                        
                 	
                 }
             	ct++;
             }
             reader.close();
-            ct--;
-            System.out.println("The total number of lines processed is " + ct);
+            ct--; 
+            System.out.println("The total number of records processed is " + ct);
             
             fileAdaptor.save(dest);
 
