4db543c1d4932f01127662d61b0d492e68ae5051
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2021-04-28 16:15:48 -0400

    Possible fix
    ...to:  org.gk.schema.InvalidAttributeException: Invalid attribute 'compartment' for class 'PhysicalEntity'.

--- src/org/gk/qualityCheck/EntitySetCompartmentCheck.java
+++ src/org/gk/qualityCheck/EntitySetCompartmentCheck.java
@@ -4,6 +4,7 @@
  */
 package org.gk.qualityCheck;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -21,50 +22,60 @@ import org.gk.persistence.MySQLAdaptor;
  *
  */
 public class EntitySetCompartmentCheck extends CompartmentCheck {
-    
+
     private static final String TOO_MANY_MEMBER_COMPARTMENTS = "More than two compartments in members";
-    
+
     private static final String COMPARTMENTS_MISMATCH = "Compartment mismatch";
 
-    public EntitySetCompartmentCheck() {        
+    public EntitySetCompartmentCheck() {
         checkClsName = ReactomeJavaConstants.EntitySet;
         followAttributes = new String[] {
                 ReactomeJavaConstants.hasMember,
                 ReactomeJavaConstants.hasCandidate
         };
     }
-    
+
     @Override
     public String getDisplayName() {
         return "Extra_Compartments_In_Entity_Set_Or_Members";
     }
-    
+
     @Override
     protected String getIssueTitle() {
         return "Extra_Compartment_DisplayNames";
     }
-    
+
     protected void loadAttributes(Collection<GKInstance> instances) throws Exception {
         MySQLAdaptor dba = (MySQLAdaptor) dataSource;
         Set<GKInstance> toBeLoaded = loadEntitySetMembers(instances, dba);
         if (progressPane != null)
             progressPane.setText("Load PhysicalEntity compartment...");
-        loadAttributes(toBeLoaded,
-                       ReactomeJavaConstants.PhysicalEntity, 
-                       ReactomeJavaConstants.compartment,
-                       dba);
+
+        for (GKInstance instance : toBeLoaded)
+        {
+            // PhysicalEntity does not have a "compartment" attribute anymore,
+            // but many subclasses may have this attribute.
+            if (instance.getSchemClass().isValidAttribute(ReactomeJavaConstants.compartment))
+            {
+                loadAttributes(Arrays.asList(instance),
+                    ReactomeJavaConstants.PhysicalEntity,
+                    ReactomeJavaConstants.compartment,
+                    dba);
+            }
+
+        }
     }
-    
+
     @Override
     protected Set<GKInstance> filterInstancesForProject(Set<GKInstance> instances) {
         return filterInstancesForProject(instances, ReactomeJavaConstants.EntitySet);
     }
-    
+
     @Override
     /**
      * Compartments used by an EntitySet's members should be the same as
      * its container EntitySet instance.
-     * 
+     *
      * @param container
      * @param containedCompartments
      * @param containerCompartments
@@ -76,7 +87,7 @@ public class EntitySetCompartmentCheck extends CompartmentCheck {
         if (containedCompartments.size() > 2) {
             return new Issue(TOO_MANY_MEMBER_COMPARTMENTS, containedCompartments);
         }
-        
+
         // Components and container should have the same number of compartments used.
         if (containerCompartments.size() != containedCompartments.size()) {
             return createMismatchIssue(containedCompartments, containerCompartments);
@@ -88,7 +99,7 @@ public class EntitySetCompartmentCheck extends CompartmentCheck {
                 return createMismatchIssue(containedCompartments, containerCompartments);
             }
         }
-        
+
         // Note: it is OK if neither the container nor the members have been assigned
         // a compartment. The mandatory checking should handle this case.
         return null;
@@ -116,22 +127,22 @@ public class EntitySetCompartmentCheck extends CompartmentCheck {
                     builder.deleteCharAt(builder.length() - 1);
                 }
         String text = builder.toString();
-        
+
         return new Issue(text);
     }
-    
+
     protected void grepCheckOutInstances(GKInstance complex,
                                          Set<GKInstance> checkOutInstances) throws Exception {
         Set<GKInstance> components = getAllContainedEntities(complex);
         checkOutInstances.addAll(components);
     }
-    
+
     protected ResultTableModel getResultTableModel() {
         ResultTableModel tableModel = new ComponentTableModel();
         tableModel.setColNames(new String[] {"Member", "Compartment"});
         return tableModel;
     }
-    
 
-    
+
+
 }
