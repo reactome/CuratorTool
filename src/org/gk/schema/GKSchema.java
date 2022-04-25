ab614547594495d5e1b178cf18d989919dba95bb
Robert Petryszak <info@datasome.co.uk>
2022-04-25 06:48:48 +0100

    Rolling back a change to initialise method, on Guanming's request

--- src/org/gk/schema/GKSchema.java
+++ src/org/gk/schema/GKSchema.java
@@ -118,11 +118,6 @@ public class GKSchema implements Schema, Serializable {
 					a.setOrigin(schemaClass);
 					setInverseAttribute(a);
 				}
-				Iterator j = schemaClasses.iterator();
-				//There should be just one element in this collection this time
-				GKSchemaClass schemaClass = (GKSchemaClass) j.next();
-				schemaClass.addAttribute(a);
-				a.setOrigin(schemaClass);
 			} else if (o instanceof GKSchemaClass) {
 				//SchemaTestApp.printObject(o);
 				addClass((GKSchemaClass) o);
