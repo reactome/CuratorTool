66e5832e04a793a4063480ea6e57119d40316471
Robert Petryszak <info@datasome.co.uk>
2022-05-09 07:49:49 +0100

    Fixed to allow the application to run on MacOSX

--- src/org/gk/osxAdapter/OSXAdapter.java
+++ src/org/gk/osxAdapter/OSXAdapter.java
@@ -6,18 +6,13 @@
 
 package org.gk.osxAdapter;
 
-// import com.apple.eawt.*;
+import org.springframework.context.ApplicationEvent;
 
 public class OSXAdapter {
-		// extends ApplicationAdapter {
-/*
-	// pseudo-singleton model; no point in making multiple instances
-	// of the EAWT application or our adapter
-	private static OSXAdapter						theAdapter;
-	private static com.apple.eawt.Application		theApplication;
+	private static OSXAdapter theAdapter;
 
 	// reference to the app where the existing quit, about, prefs code is
-	private OSXApplication									mainApp;
+	private OSXApplication mainApp;
 	
 	private OSXAdapter (OSXApplication inApp) {
 		mainApp = inApp;
@@ -27,7 +22,6 @@ public class OSXAdapter {
 	// functionality from the main app, as if it came over from another platform.
 	public void handleAbout(ApplicationEvent ae) {
 		if (mainApp != null) {
-			ae.setHandled(true);
 			mainApp.about();
 		} else {
 			throw new IllegalStateException("handleAbout: OSXApplication instance detached from listener");
@@ -37,7 +31,6 @@ public class OSXAdapter {
 	public void handlePreferences(ApplicationEvent ae) {
 		if (mainApp != null) {
 			mainApp.preferences();
-			ae.setHandled(true);
 		} else {
 			throw new IllegalStateException("handlePreferences: OSXApplication instance detached from listener");
 		}
@@ -45,14 +38,6 @@ public class OSXAdapter {
 	
 	public void handleQuit(ApplicationEvent ae) {
 		if (mainApp != null) {
-
-			/	You MUST setHandled(false) if you want to delay or cancel the quit.
-			/	This is important for cross-platform development -- have a universal quit
-			/	routine that chooses whether or not to quit, so the functionality is identical
-			/	on all platforms.  This example simply cancels the AppleEvent-based quit and
-			/	defers to that universal method.
-
-			ae.setHandled(false);
 			mainApp.quit();
 		} else {
 			throw new IllegalStateException("handleQuit: OSXApplication instance detached from listener");
@@ -64,23 +49,14 @@ public class OSXAdapter {
 	// that needs to be called at runtime, and it can easily be done using
 	// reflection (see MyApp.java) 
 	public static void registerMacOSXApplication(OSXApplication inApp) {
-		if (theApplication == null) {
-			theApplication = new com.apple.eawt.Application();
-		}			
-		
 		if (theAdapter == null) {
 			theAdapter = new OSXAdapter(inApp);
 		}
-		theApplication.addApplicationListener(theAdapter);
 	}
 	
 	// Another static entry point for EAWT functionality.  Enables the 
 	// "Preferences..." menu item in the application menu. 
 	public static void enablePrefs(boolean enabled) {
-		if (theApplication == null) {
-			theApplication = new com.apple.eawt.Application();
-		}
-		theApplication.setEnabledPreferencesMenu(enabled);
+		// TODO
 	}
-	*/
 }
