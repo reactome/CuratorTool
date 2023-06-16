82e88d1056106ee939824d08d611fd3db26a715e
guanmingwu <guanmingwu@gmail.com>
2023-06-16 12:18:44 -0700

    build notes for 116

--- src/org/gk/gkCurator/GKCuratorFrame.java
+++ src/org/gk/gkCurator/GKCuratorFrame.java
@@ -99,8 +99,8 @@ public class GKCuratorFrame extends JFrame implements Launchable {
     // System info
     public static final String CURATOR_TOOL_NAME = "Reactome Curator Tool";
     public static final String PROJECT_EXT_NAME = ".rtpj";
-    public static final String VERSION = "4.0.2 (May 8, 2023)";
-    public static final int BUILD_NUMBER = 115;
+    public static final String VERSION = "4.0.2 (June 16, 2023)";
+    public static final int BUILD_NUMBER = 116;
     static final String QA_MENU_TEXT = "QA Check";
     // For tab title
     private final String PROJECT_TITLE = "Event Hierarchical View";
